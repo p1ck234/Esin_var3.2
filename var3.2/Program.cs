35322c59eb9f41eea4385a94f70fc008c4a9bc6f
Danil <93711640+p1ck234@users.noreply.github.com>
2022-12-09 17:51:34 +0400

    upl

---
+++ var3.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List < List < int>> main = new List<List<int>>();
+            List<int> first = new List<int>() { 1, 2, 3, 4 ,5 };
+            List<int> second = new List<int>() { 2, 3, 4, 5 ,6 };
+            List<int> third = new List<int>() { 3, 4, 5, 6, 7 };
+
+            main.Add(first);
+            main.Add(second);
+            main.Add(third);
+
+            List<int> max = new List<int>();
+
+            for (int i = 0; i < main.Count; i++)
+            {
+                max.Add(main[i].Max());
+            }
+
+            Console.Write($"List<int> ");
+            for (int i = 0; i < max.Count; i++)
+            {
+                Console.Write(max[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
